c08e1056f216b6b05c04a4f5de3df54399d381b6
Olonets13 <st9103748@stud.duikt.edu.ua>
2026-03-15 22:25:57 +0200

    Add files via upload

---
+++ PZ1C#Task5/PZ1C#Task5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FlexibleLogger
+{
+    public class Logger
+    {
+        public Action<string> LogHandler;
+
+        public void Log(string message)
+        {
+            LogHandler?.Invoke(message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Logger myLogger = new Logger();
+
+            myLogger.LogHandler = message => Console.WriteLine($"[Console Log]: {message}");
+
+            Console.WriteLine("--- Перший запуск ---");
+            myLogger.Log("Програма розпочала роботу.");
+
+            myLogger.LogHandler = message =>
+            {
+                string upperMessage = message.ToUpper();
+                Console.WriteLine($"[UPPER LOG]: {upperMessage}");
+            };
+
+            Console.WriteLine("\n--- Другий запуск (після зміни логіки) ---");
+            myLogger.Log("важливе сповіщення про помилку!");
+
+            myLogger.LogHandler += message => Console.WriteLine($"[Length]: Довжина повідомлення - {message.Length}");
+
+            Console.WriteLine("\n--- Третій запуск (мультикаст) ---");
+            myLogger.Log("Тестування завершено.");
+
+            Console.ReadKey();
+        }
+    }
+}
